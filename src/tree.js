93fcc753da355f82b3aa77b042508bc1267983e0
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-03-18 19:08:12 -0400

    Add deleteItem() method

--- src/tree.js
+++ src/tree.js
@@ -70,5 +70,85 @@ export default class Tree {
         currentNode = currentNode.right;
       }
     }
+    // Rebalance
+  }
+
+  deleteItem(value, currentNode = this.root) {
+    if (currentNode === null) return null;
+    if (
+      currentNode.data === value &&
+      currentNode.left === null &&
+      currentNode.right === null
+    ) {
+      return null;
+    }
+    if (
+      currentNode.data === value &&
+      currentNode.left != null &&
+      currentNode.right === null
+    ) {
+      currentNode.data = currentNode.left.data;
+      currentNode.left = null;
+      return currentNode;
+      // rebalance
+    }
+    if (
+      currentNode.data === value &&
+      currentNode.left === null &&
+      currentNode.right != null
+    ) {
+      currentNode.data = currentNode.right.data;
+      currentNode.right = null;
+      return currentNode;
+      // rebalance
+    }
+    if (
+      currentNode.data === value &&
+      currentNode.left != null &&
+      currentNode.right != null
+    ) {
+      const nextLowestValue = this.findNextLowestNode(currentNode.data).data;
+      this.deleteItem(nextLowestValue);
+      currentNode.data = nextLowestValue;
+      // rebalance
+    }
+
+    if (value < currentNode.data) {
+      currentNode.left = this.deleteItem(value, currentNode.left);
+    } else if (value > currentNode.data) {
+      currentNode.right = this.deleteItem(value, currentNode.right);
+    }
+    return currentNode;
+  }
+
+  find(value, node = this.root) {
+    if (node === null) {
+      return null;
+    }
+    let nodeFound;
+    if (node.data === value) {
+      nodeFound = node;
+    } else {
+      const checkLeft = this.find(value, node.left);
+      const checkRight = this.find(value, node.right);
+      if (checkLeft != null) {
+        nodeFound = checkLeft;
+      } else if (checkRight != null) {
+        nodeFound = checkRight;
+      }
+    }
+
+    return nodeFound;
+  }
+
+  findNextLowestNode(value, node = this.find(value)) {
+    if (node === null) return null;
+    if (node.data === value && node.left === null && node.right != null)
+      return node.right;
+    if (node.left === null) {
+      return node;
+    }
+    const nextLowestNode = this.findNextLowestNode(value, node.left);
+    return nextLowestNode;
   }
 }
